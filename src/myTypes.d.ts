af1eb9eb20e2602886d73490d8844ebe53069213
HuyiCui <cuihuyi512@gmail.com>
2025-02-06 00:23:30 +0000

    Demo optional properties and parameters.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -1,8 +1,11 @@
 export interface Friend {
-    name: string;
-    phone: string;
-    age: number
+  name: string;
+  phone: string;
+  dob?: Date;        
+  age: number;
+  interests?: string[]; 
 }
+
 
 export interface Colleague {
     name: string;
